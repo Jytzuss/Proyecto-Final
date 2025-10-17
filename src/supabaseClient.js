8efc63cdc926ec8a1fb6e7c900c8d06e69d9ef05
Alisson Orozco <jytzus@gmail.com>
2025-10-16 22:48:32 -0500

    Ajuste en supabaseclient

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 
+
+const redirectUrl =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5173/home"
+    : "https://proyecto-final-blond-ten.vercel.app/home";
+
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      redirectTo: redirectUrl,
+    },
+  }
 );
 
 export default supabase;
